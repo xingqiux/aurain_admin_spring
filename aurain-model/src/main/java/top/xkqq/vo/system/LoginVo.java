731412efb9333a6e49417dde4447003393d3a1af
子雨 <ziyurushang@gmail.com>
2025-03-06 20:29:47 +0800

    feat(aurain-manager): 添加用户退出功能并优化登录流程
    - 新增用户退出接口和相关逻辑
    - 优化登录接口，移除不必要的响应字段
    - 重构验证码生成和验证逻辑
    - 更新用户信息获取方法
    - 添加登录认证拦截器

--- aurain-model/src/main/java/top/xkqq/vo/system/LoginVo.java
+++ aurain-model/src/main/java/top/xkqq/vo/system/LoginVo.java
@@ -9,27 +9,10 @@ import lombok.Data;
 @Data
 @Schema(description = "登陆成功后响应实体类")
 public class LoginVo {
-//    @Schema(description = "令牌")
-//    private String token;
-//    @Schema(description = "刷新令牌，可以为空")
-//    private String refreshToken;   //该字段不会存储对应值
 
-    /**
-     * 返回符合前端要求的参数
-     *   {
-     *     "accessToken": "string",    // JWT访问令牌
-     *     "refreshToken": "string",   // 刷新令牌
-     *     "tokenType": "string",      // 令牌类型，通常为"Bearer"
-     *     "expiresIn": number         // 访问令牌有效期(秒)
-     *   }
-     */
     @Schema(description = "JWT访问令牌")
-    private String accessToken;
+    private String token ;
     @Schema(description = "刷新令牌")
-    private String refreshToken;
-    @Schema(description = "令牌类型，通常为\"Bearer")
-    private String tokenType;
-    @Schema(description = "访问令牌有效期(秒)")
-    private Integer expiresIn;
+    private String refresh_token ;		// 该字段不会存储对应的值
 
 }
