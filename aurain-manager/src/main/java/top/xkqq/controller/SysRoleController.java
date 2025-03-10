d817f3d90eacce6d39bf5e3f5917e6b15ca40e02
子雨 <ziyurushang@gmail.com>
2025-03-10 16:42:09 +0800

    feat(aurain-manager):集成 MyBatis Plus 并优化 SysRole 相关功能 - 在 application-dev.yml 中添加 MyBatis Plus 配置 - 更新 pom.xml 文件，添加 MyBatis Plus 相关依赖 - 修改 SysRoleController，增加 save、update 和 delete 方法 - 更新 SysRoleServiceImpl，实现新增的 SysRole 相关方法 - 在 BaseEntity 中添加逻辑删除字段注解 - 更新项目结构，移除未使用的代码

--- aurain-manager/src/main/java/top/xkqq/controller/SysRoleController.java
+++ aurain-manager/src/main/java/top/xkqq/controller/SysRoleController.java
@@ -5,15 +5,12 @@ import com.github.pagehelper.PageInfo;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import top.xkqq.dto.SysRoleDto;
 import top.xkqq.entity.system.SysRole;
 import top.xkqq.service.SysRoleService;
 import top.xkqq.vo.common.Result;
 import top.xkqq.vo.common.ResultCodeEnum;
-
-import java.util.Objects;
 
 @Tag(name = " 分组接口")
 @RestController
@@ -24,7 +21,7 @@ public class SysRoleController {
     private SysRoleService sysRoleService;
 
 
-    @Operation(summary = "访问分页的findByPage")
+    @Operation(summary = "访问分页 findByPage")
     @PostMapping("/findByPage/{current}/{pageSize}")
     public Result<SysRole> findByPage(@PathVariable("current") Integer current,
                              @PathVariable("pageSize") Integer pageSize,
@@ -33,8 +30,28 @@ public class SysRoleController {
 
         PageInfo<SysRole> pageInfo = sysRoleService.findByPage(sysRoleDto ,current , pageSize) ;
         return Result.build(pageInfo, ResultCodeEnum.SUCCESS);
-
-
     }
 
+    @Operation(summary = "添加权限 saveSysRole")
+    @PostMapping("/saveSysRole")
+    public Result<SysRole> saveSysRole(@RequestBody SysRole sysRole) {
+        sysRoleService.save(sysRole);
+        return Result.build(sysRole, ResultCodeEnum.SUCCESS);
+    }
+
+    @Operation(summary = "修改权限 updateSysRole")
+    @PutMapping("/updateSysRole")
+    public Result<SysRole> updateSysRole(@RequestBody SysRole sysRole) {
+        sysRoleService.updateById(sysRole);
+        return Result.build(sysRole, ResultCodeEnum.SUCCESS);
+    }
+
+    @Operation(summary = "删除权限用户 deleteSysRole")
+    @DeleteMapping("/deleteById/{id}")
+    public Result<SysRole> deleteSysRole(@PathVariable("id") Long id) {
+        System.out.println(id);
+        boolean b = sysRoleService.removeById(id);
+        System.out.println("b = " + b);
+        return Result.build(null, ResultCodeEnum.SUCCESS);
+    }
 }
--- aurain-model/src/main/java/top/xkqq/entity/base/BaseEntity.java
+++ aurain-model/src/main/java/top/xkqq/entity/base/BaseEntity.java
@@ -1,5 +1,6 @@
 package top.xkqq.entity.base;
 
+import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
@@ -14,6 +15,7 @@ public class BaseEntity implements Serializable {
 
     // 此类中定义公共的属性
     @Schema(description = "唯一标识")
+    @JsonFormat(shape = JsonFormat.Shape.STRING)    // 用于解决前端不能正确处理后端雪花算法id问题
     private Long id;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @Schema(description = "创建时间")
@@ -21,6 +23,8 @@ public class BaseEntity implements Serializable {
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @Schema(description = "修改时间")
     private Date updateTime;
+
+    @TableLogic(value = "0", delval = "1")
     @Schema(description = "是否删除")
     private Integer isDeleted;
 
